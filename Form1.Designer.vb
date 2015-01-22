b6fef5615c488c2a92a0508c15a118dae00c6256
Maya Minatsuki <mayaeh@marimo-net.org>
2015-01-22 14:08:34 +0900

    1st commit.

---
+++ Form1.Designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem2 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("メイリオ", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label1.Location = New System.Drawing.Point(0, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(140, 31)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "ログイン中："
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("メイリオ", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label2.Location = New System.Drawing.Point(133, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(87, 31)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Label2"
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "NotifyIcon1"
+        Me.NotifyIcon1.Visible = True
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItem2, Me.ToolStripMenuItem1})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(153, 70)
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(152, 22)
+        Me.ToolStripMenuItem1.Text = "終了"
+        Me.ToolStripMenuItem1.ToolTipText = "このアプリケーションを終了します"
+        '
+        'ToolStripMenuItem2
+        '
+        Me.ToolStripMenuItem2.Name = "ToolStripMenuItem2"
+        Me.ToolStripMenuItem2.Size = New System.Drawing.Size(152, 22)
+        Me.ToolStripMenuItem2.Text = "情報"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.ClientSize = New System.Drawing.Size(284, 42)
+        Me.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
+        Me.Text = "Form1"
+        Me.TopMost = True
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents NotifyIcon1 As System.Windows.Forms.NotifyIcon
+    Friend WithEvents ContextMenuStrip1 As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents ToolStripMenuItem1 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem2 As System.Windows.Forms.ToolStripMenuItem
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,118 @@
+﻿Public Class Form1
+
+    Public Sub New()
+
+        ' この呼び出しはデザイナーで必要です。
+        InitializeComponent()
+
+        ' InitializeComponent() 呼び出しの後で初期化を追加します。
+
+        ' タイトルバーを消す
+        Me.FormBorderStyle = Windows.Forms.FormBorderStyle.None
+
+
+        ' ディスプレイサイズを取得し、ウインドウの表示位置を右下に設定する
+        Dim screenH, screenW As Integer
+        screenH = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height
+        screenW = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width
+
+        Me.Location = New Point(screenW - My.Settings.MarginRight, screenH - My.Settings.MarginBottom)
+
+        ' ウインドウサイズを変更する
+        Me.Size = New Size(My.Settings.FormWidth, My.Settings.FormHeight)
+
+
+        ' フォントと背景色を変更する
+        Me.Label1.Font = New Font(My.Settings.FontName, My.Settings.FontSize)
+        Me.Label1.ForeColor = Color.FromName(My.Settings.FontColor)
+
+        Me.Label2.Font = New Font(My.Settings.FontName, My.Settings.FontSize)
+        Me.Label2.ForeColor = Color.FromName(My.Settings.FontColor)
+
+        ' 右クリックメニューのフォントを変更する
+        Me.ContextMenuStrip1.Font = New Font(My.Settings.MenuFontName, My.Settings.MenuFontSize)
+
+        Me.BackColor = Color.FromName(My.Settings.BackgroundColor)
+
+        ' もしラベル１を非表示にする設定なら非表示にする
+        If My.Settings.ShowLabel1 = False Then
+            Me.Label1.Visible = False
+            Me.Label2.Left = 10
+        End If
+
+
+        ' ##### ユーザー名・ドメイン名を取得し DirectoryService に問い合わせ、フルネームを取得する #####
+
+        Dim uName As String = Environment.UserName
+        Dim domain As String = Environment.UserDomainName
+
+        ' ユーザー名 / ドメイン名 を出力
+        'Me.Label2.Text = uName & " / " & domain
+
+        Dim path As String = "WinNT://" & domain & "/" & uName
+
+        Dim dirEnt As System.DirectoryServices.DirectoryEntry = New System.DirectoryServices.DirectoryEntry(path)
+
+        Dim dName As String = dirEnt.Properties("FullName").Value.ToString()
+
+        ' 空欄の場合はユーザー名を表示する
+        If dName = "" Then
+            Me.Label2.Text = uName
+
+        Else
+            Me.Label2.Text = dName
+        End If
+
+
+    End Sub
+
+
+
+
+    ' 終了ボタン
+    Private Sub ToolStripMenuItem1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripMenuItem1.Click
+        Application.Exit()
+    End Sub
+
+    ' 情報ボタン
+    Private Sub ToolStripMenuItem2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripMenuItem2.Click
+        AboutBox1.Show()
+    End Sub
+
+
+    ' ##### タイトルバーのないウインドウでもドラッグ可能にする #####
+
+    ' マウスのクリック位置を記憶
+    Private mousePoint As Point
+
+    ' Form1 の MouseDown イベントハンドラ
+    Private Sub Form1_MouseDown(ByVal ByValsender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles MyBase.MouseDown
+        If (e.Button And MouseButtons.Left) = MouseButtons.Left Then
+            ' 位置を記憶する
+            mousePoint = New Point(e.X, e.Y)
+        End If
+    End Sub
+
+    ' Form1 の MouseMove イベントハンドラ
+    Private Sub Form1_MouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles MyBase.MouseMove
+        If (e.Button And MouseButtons.Left) = MouseButtons.Left Then
+            Me.Location = New Point( _
+                Me.Location.X + e.X - mousePoint.X, _
+                Me.Location.Y + e.Y - mousePoint.Y)
+
+        End If
+    End Sub
+
+
+
+    ' タスクトレイのアイコンをクリック・ダブルクリックした時も右クリックのメニューを表示させる
+    Private Sub NotifyIcon1_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles NotifyIcon1.Click
+        Me.ContextMenuStrip1.Show(System.Windows.Forms.Cursor.Position)
+    End Sub
+
+    Private Sub NotifyIcon1_DoubleClick(ByVal sender As Object, ByVal e As System.EventArgs) Handles NotifyIcon1.DoubleClick
+        Me.ContextMenuStrip1.Show(System.Windows.Forms.Cursor.Position)
+    End Sub
+
+
+End Class
