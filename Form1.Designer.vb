dac1e808d5a55522eb525b2b8909f82ae195b5a0
Maya Minatsuki <mayaeh@marimo-net.org>
2016-11-23 08:56:08 +0900

    Modified TaskTray Icon tooltip.

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -61,8 +61,8 @@ Partial Class Form1
         '
         Me.NotifyIcon1.ContextMenuStrip = Me.ContextMenuStrip1
         Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
-        Me.NotifyIcon1.Text = "NotifyIcon1"
-        Me.NotifyIcon1.Visible = True
+		Me.NotifyIcon1.Text = "fullNameGetter"
+		Me.NotifyIcon1.Visible = True
         '
         'ContextMenuStrip1
         '
